5fa4c10264c008b0db527638fd0f5568a5a44b3c
Daniela Brozzoni <danielabrozzoni@protonmail.com>
2023-11-14 14:38:11 +0100

    Fix cargo clippy

--- build.rs
+++ build.rs
@@ -3,13 +3,13 @@ use std::process::Command;
 // Inspired by https://stackoverflow.com/questions/43753491/include-git-commit-hash-as-string-into-rust-program
 fn main() {
     let git_status = Command::new("git")
-        .args(&["rev-parse", "HEAD"])
+        .args(["rev-parse", "HEAD"])
         .output()
         .ok()
         .and_then(|output| String::from_utf8(output.stdout).ok())
         .zip(
             Command::new("git")
-                .args(&["diff-files", "--quiet"])
+                .args(["diff-files", "--quiet"])
                 .status()
                 .ok()
                 .map(|status| if status.success() { "" } else { "-dirty" }),
